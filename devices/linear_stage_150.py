c9481df8e74fbfdf132af689b9d28c85a96cfaa6
Piyush <ppahuja2@illinois.edu>
2023-05-22 17:23:56 -0500

    added lts150 absolute move methods

--- devices/linear_stage_150.py
+++ devices/linear_stage_150.py
@@ -4,8 +4,12 @@ import time
 from .device import SerialDevice, check_serial, check_initialized
 
 class LinearStage150(SerialDevice):
-    def __init__(self, name: str, port: str = '"COM9"', baudrate: int = 115200, timeout: float | None = 0.1):
+    def __init__(self, name: str, port: str = '\'/dev/cu.URT0\'', baudrate: int = 115200, timeout: float | None = 0.1, destination: int = 0x50, source: int = 0x01, channel: int = 1):
         super().__init__(name, port, baudrate, timeout)
+        self._destination = destination
+        self._source = source
+        self._channel = channel
+        print("dest: "+str(self._destination))
     
     @check_serial
     def initialize(self) -> Tuple[bool, str]:
@@ -36,19 +40,33 @@ class LinearStage150(SerialDevice):
     
     def set_enabled_state(self,  state: bool) -> Tuple[bool, str]:
         if state:
-            self.ser.write(pack('<HBBBB',0x0210,1,0x01,0x50,0x01))
+            self.ser.write(pack('<HBBBB',0x0210,1,0x01,self._destination,self._source))
         else:
             self.ser.write(pack('<HBBBB',0x0210,1,0x02,0x50,0x01))
         time.sleep(0.1)
-        #TODO: lts150 set enable state
-
+        #TODO-VERIFY: (verify) lts150 set enable state, MGMSG_MOD_SET_CHANENABLESTATE
+        #TODO: check if flushing is needed
         return (True, "Successfully set enable state to " + str(state) + ".")
 
     @check_serial
-    @check_initialized #TODO: lts150: check is initialized required for absolute movement
+    @check_initialized #TODO: lts150: check if initialized required for absolute movement
     def move_absolute(self, position: float) -> Tuple[bool, str]:
-        
-        #TODO: lts150 move absolute, MGMSG_MOT_MOVE_ABSOLUTE
+        Device_Unit_SF = 409600
+        dUnitpos = int(Device_Unit_SF*position)
+        self.ser.write(pack('<HBBBBHI',0x0453,0x06,0x00,self._destination|0x80,self._source,self._channel,dUnitpos))
+
+        #Confirm stage completed move before advancing; MGMSG_MOT_MOVE_COMPLETED 
+        Rx = ''
+        Moved = pack('<H',0x0464)
+        while Rx != Moved:
+            Rx = self.ser.read(2)
+
+        print('Move Complete')
+
+        self.ser.flushInput()
+        self.ser.flushOutput()
+
+        #TODO-VERIFY: (verify) lts150 move absolute, MGMSG_MOT_MOVE_ABSOLUTE
 
         return (True, "Successfully moved stage to position " + str(position) + "[units].")
 

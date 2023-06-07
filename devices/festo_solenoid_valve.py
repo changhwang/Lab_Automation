90d7512a0a5982ee24b6a77804c55054069bb850
nikhiljjose <nikhiljjose@gmail.com>
2023-06-07 09:01:21 -0500

    Added solenoid control

--- devices/festo_solenoid_valve.py
+++ devices/festo_solenoid_valve.py
@@ -1,25 +1,45 @@
+"""Requires 'StandardFirmata' basic example uploaded on Arduino Uno"""
 from typing import Tuple, Optional
-import serial
+import time
+import pyfirmata
 
-from .device import ArduinoSerialDevice, check_initialized, check_serial
+from device import ArduinoSerialDevice, check_initialized, check_serial
 
 class FestoSolenoidValve(ArduinoSerialDevice):
 
-    def __init__(self, name: str, port: str, baudrate: int, timeout: float):
+    def __init__(self, name: str, numchannel: int, port: str = "COM5", baudrate: int = 9600, timeout: float = 0.1):
         super().__init__(name, port, baudrate, timeout)
+        self.board = pyfirmata.Arduino(self.port)
+        self.pin = self.board.get_pin(f"d:{numchannel}:o")
     
     def initialize(self) -> Tuple[bool, str]:
         self._is_initialized = True
-        #TODO solenoid valve initialize
+        #TODO: solenoid valve initialize
         return (True, "Solenoid valve initialized")
     
     def deinitialize(self) -> Tuple[bool, str]:
         self._is_initialized = False
-        #TODO solenoid valve deinitialize
+        self.board.exit()
         return (True, "Solenoid valve deinitialized")
 
     @check_serial
     @check_initialized
     def valve_open(self) -> Tuple[bool, str]:
-        #TODO activate solenoid valve
+        self.pin.write(1)
         return (True, "Solenoid valve is open")
+    
+    def valve_closed(self) -> Tuple[bool, str]:
+        self.pin.write(0)
+        return (True, "Solenoid valve is closed")
+    
+    def open_timed(self, time: int) -> Tuple[bool, str]:
+        self.pin.write(1)
+        self.board.pass_time(time)
+        self.pin.write(0)
+        return (True, f"Solenoid valve was opened for {time} seconds")
+
+test = FestoSolenoidValve('tester', 13)
+#test.initialize()
+test.valve_open()
+
+    

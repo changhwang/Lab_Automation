7c9b769fe1d5bb76bcbca6a48ff5efce91dc2809
nikhiljjose <nikhiljjose@gmail.com>
2023-07-28 12:26:13 -0500

    updated festo valve control

--- commands/festo_solenoid_valve_commands.py
+++ commands/festo_solenoid_valve_commands.py
@@ -1,5 +1,4 @@
 from typing import List
-
 from devices.festo_solenoid_valve import FestoSolenoidValve
 from .command import Command, CommandResult
 
@@ -9,6 +8,16 @@ class FestoParentCommand(Command):
 
     def __init__(self, receiver: FestoSolenoidValve, **kwargs):
         super().__init__(receiver, **kwargs)
+
+class FestoConnect(Command):
+    """Open the serial port to the festo valve"""
+    receiver_cls = FestoSolenoidValve
+
+    def __init__(self, receiver: FestoSolenoidValve, **kwargs):
+        super().__init__(receiver, **kwargs)
+
+    def execute(self) -> None:
+        self._result = CommandResult(*self._receiver.start_serial())
 
 class FestoInitialize(FestoParentCommand):
     """Initialize the solenoid valve"""
@@ -31,28 +40,21 @@ class FestoDeinitialize(FestoParentCommand):
 class FestoValveOpen(FestoParentCommand):
     """Open the solenoid valve and keep open"""
 
-    def __init__(self, receiver: FestoSolenoidValve, **kwargs):
+    def __init__(self, receiver: FestoSolenoidValve, valve_num: int, **kwargs):
         super().__init__(receiver, **kwargs)
+        self._params['valve_num'] = valve_num
 
     def execute(self) -> None:
-        self._result = CommandResult(*self._receiver.valve_open())
+        self._result = CommandResult(*self._receiver.valve_open(self._params['valve_num']))
 
 class FestoValveClosed(FestoParentCommand):
     """Close the solenoid valve and keep closed"""
 
-    def __init__(self, receiver: FestoSolenoidValve, **kwargs):
+    def __init__(self, receiver: FestoSolenoidValve, valve_num: int, **kwargs):
         super().__init__(receiver, **kwargs)
+        self._params['valve_num'] = valve_num
 
     def execute(self) -> None:
-        self._result = CommandResult(*self._receiver.valve_closed())
+        self._result = CommandResult(*self._receiver.valve_closed(self._params['valve_num']))
 
-class FestoOpenTimed(FestoParentCommand):
-    """Open the valve for a set time then close"""
-
-    def __init__(self, receiver: FestoSolenoidValve, time: int, **kwargs):
-        super().__init__(receiver, **kwargs)
-        self._params['time'] = time
-    
-    def execute(self) -> None:
-        self._result = CommandResult(*self._receiver.open_timed(self._params['time']))
 
